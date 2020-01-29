2f3205ad41acb82073e8a9a3f66b331790967bca
mliraudcoel <melanie.liraud@gmail.com>
2020-01-29 14:51:58 +0100

    data injected in database

--- install.sql
+++ install.sql
@@ -33,3 +33,11 @@ CREATE TABLE `show`(
   `date` DATETIME
 );
 
+
+INSERT INTO `performance`(title, description) VALUES ('Laugh', 'As an adult, come and discover our irresistible clowns, between practical jokes and pranks let yourself be carried away by their joy and fall back into childhood.');
+INSERT INTO `performance`(title, description) VALUES ('Dream', 'Let yourself be carried away in a world where the real and the imaginary are one, in the company of our talented magicians, discover a wonderful world limited only by your imagination.');
+INSERT INTO `performance`(title, description) VALUES ('Marvel at', 'Tame the untameable in the company of our tamers, between roar and razor-sharp claws, watch these fierce felines turn into sweet kittens.');
+INSERT INTO `price`(name, adult_price, child_price, group_price, school_price) VALUES ('Weeks except wednesday', 18, 12, 9, 6);
+INSERT INTO `price`(name, adult_price, child_price, group_price, school_price) VALUES ('Week-end and wednesday', 22, 16, 13, 10);
+INSERT INTO `team`(name) VALUES ('Clowns'), ('Magicians'), ('Tamers');
+
